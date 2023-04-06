d31ac3abab415acd8a2ef31f4f06ed3330999979
andie <andiethealto@gmail.com>
2023-04-06 11:43:24 -0400

    Added "new" button to layout

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.temple.viewpagerinclasssctivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -12,9 +13,16 @@ class MainActivity : AppCompatActivity() {
     val viewPager : ViewPager2 by lazy{
         findViewById(R.id.viewPager)
     }
+    val newButton : Button by lazy{
+        findViewById(R.id.button)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        newButton.setOnClickListener{
+
+        }
 
         viewPager.adapter = object: FragmentStateAdapter(this) {
             override fun getItemCount() = 10
